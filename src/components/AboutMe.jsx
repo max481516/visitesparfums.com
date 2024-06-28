6c6e6679514eaf7079482363b12fe0b93489136a
Maximilien Brunet <maximilien.brunet@me.com>
2024-06-28 16:16:13 +0200

    broken page fixed

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -39,26 +39,3 @@ const Image = styled.img`
 const Text = styled.p`
   padding: 8px;
 `;
-
-
-Natacha – From Moscow to Paris
-
-I'm Natacha, a passionate perfume enthusiast based in Paris. Growing up in a multicultural environment with dual French-Russian nationality, I have always been inspired by my encounters and life experiences, leading me to a creative career. With a strong background in olfactory arts, I bring a unique and personalized approach to the world of scents.
-
-From Perfume Lover to Fragrance Nerd
-
-My journey into perfumes began with my mum, who introduced me to niche fragrance boutiques, making my encounter with perfumes of Serge Lutens and Frederic Malle unfrogettable. As a student, I worked for prestigious French brands like Fragonard and Le Labo, appreciating the customer interactions and the dialogue about fragrances. I am eternally impressed by their ability to evoke emotions and memories.
-
-Today, I live, breathe, and sleep fragrances.
-
-My Experience at Your Service
-
-Over the years, I’ve honed my skills and knowledge, working for fragrances brands like Jimmy Choo and fragrance houses such as Givaudan, IFF, and Sozio. I am dedicated to elevating the fragrance experience for every customer I meet. This world is full of secrets, creativity, and curiosity - and I am passionate about sharing it with others.
-
-Let’s Make the Magic Happen
-
-I believe that a fragrance should be as unique as the person wearing it. Each scent tells a story, captures a moment, and expresses individuality. My goal is to help you discover the perfect scent that resonates with your personality and style, making you feel confident and inspired every day.
-
- 
-
-Ready to dive into the world of fragrances? Reach out and let's book a tour!
