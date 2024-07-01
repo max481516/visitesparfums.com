7751efc7ffa2b97438b3e251257adaeb9dc2f122
Maximilien Brunet <maximilien.brunet@me.com>
2024-07-01 17:26:32 +0200

    AboutMe optimized for laptop

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -17,55 +17,63 @@ export default function AboutMe() {
       <Title>{t("About Me")}</Title>
       <ItemContainer>
         <Image src={AboutMePhoto1} alt="aboutme" />
-        <ItemTitle>Natacha - From Moscow to Paris</ItemTitle>
-        <Text>
-          I&apos;m Natacha, a passionate perfume enthusiast based in Paris.
-          Growing up in a multicultural environment with dual French-Russian
-          nationality, I have always been inspired by my encounters and life
-          experiences, leading me to a creative career. With a strong background
-          in olfactory arts, I bring a unique and personalized approach to the
-          world of scents.
-        </Text>
+        <ItemTextContainer>
+          <ItemTitle>Natacha - From Moscow to Paris</ItemTitle>
+          <Text>
+            I&apos;m Natacha, a passionate perfume enthusiast based in Paris.
+            Growing up in a multicultural environment with dual French-Russian
+            nationality, I have always been inspired by my encounters and life
+            experiences, leading me to a creative career. With a strong
+            background in olfactory arts, I bring a unique and personalized
+            approach to the world of scents.
+          </Text>
+        </ItemTextContainer>
       </ItemContainer>
       <ItemContainer>
         <Image src={AboutMePhoto2} alt="aboutme" />
-        <ItemTitle>From Perfume Lover to Fragrance Nerd</ItemTitle>
-        <Text>
-          My journey into perfumes began with my mum, who introduced me to niche
-          fragrance boutiques, making my encounter with perfumes of Serge Lutens
-          and Frederic Malle unfrogettable. As a student, I worked for
-          prestigious French brands like Fragonard and Le Labo, appreciating the
-          customer interactions and the dialogue about fragrances. I am
-          eternally impressed by their ability to evoke emotions and memories.
-          Today, I live, breathe, and sleep fragrances.
-        </Text>
+        <ItemTextContainer>
+          <ItemTitle>From Perfume Lover to Fragrance Nerd</ItemTitle>
+          <Text>
+            My journey into perfumes began with my mum, who introduced me to
+            niche fragrance boutiques, making my encounter with perfumes of
+            Serge Lutens and Frederic Malle unfrogettable. As a student, I
+            worked for prestigious French brands like Fragonard and Le Labo,
+            appreciating the customer interactions and the dialogue about
+            fragrances. I am eternally impressed by their ability to evoke
+            emotions and memories. Today, I live, breathe, and sleep fragrances.
+          </Text>
+        </ItemTextContainer>
       </ItemContainer>
       <ItemContainer>
         <Image src={AboutMePhoto3} alt="aboutme" />
-        <ItemTitle>My Experience at Your Service</ItemTitle>
-        <Text>
-          Over the years, I&apos;ve honed my skills and knowledge, working for
-          fragrances brands like Jimmy Choo and fragrance houses such as
-          Givaudan, IFF, and Sozio. I am dedicated to elevating the fragrance
-          experience for every customer I meet. This world is full of secrets,
-          creativity, and curiosity - and I am passionate about sharing it with
-          others.
-        </Text>
+        <ItemTextContainer>
+          <ItemTitle>My Experience at Your Service</ItemTitle>
+          <Text>
+            Over the years, I&apos;ve honed my skills and knowledge, working for
+            fragrances brands like Jimmy Choo and fragrance houses such as
+            Givaudan, IFF, and Sozio. I am dedicated to elevating the fragrance
+            experience for every customer I meet. This world is full of secrets,
+            creativity, and curiosity - and I am passionate about sharing it
+            with others.
+          </Text>
+        </ItemTextContainer>
       </ItemContainer>
       <ItemContainer>
         <Image src={AboutMePhoto4} alt="aboutme" />
-        <ItemTitle>Let&apos;s Make the Magic Happen</ItemTitle>
-        <Text>
-          I believe that a fragrance should be as unique as the person wearing
-          it. Each scent tells a story, captures a moment, and expresses
-          individuality. My goal is to help you discover the perfect scent that
-          resonates with your personality and style, making you feel confident
-          and inspired every day.
-        </Text>
-        <Conclusion>
-          Ready to dive into the world of fragrances? Reach out and let&apos;s{" "}
-          <BookLink to="/booking">book a tour!</BookLink>
-        </Conclusion>
+        <ItemTextContainer>
+          <ItemTitle>Let&apos;s Make the Magic Happen</ItemTitle>
+          <Text>
+            I believe that a fragrance should be as unique as the person wearing
+            it. Each scent tells a story, captures a moment, and expresses
+            individuality. My goal is to help you discover the perfect scent
+            that resonates with your personality and style, making you feel
+            confident and inspired every day.
+          </Text>
+          <Conclusion>
+            Ready to dive into the world of fragrances? Reach out and let&apos;s{" "}
+            <BookLink to="/booking">book a tour!</BookLink>
+          </Conclusion>
+        </ItemTextContainer>
       </ItemContainer>
     </Wrapper>
   );
@@ -75,10 +83,12 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   padding: 0.5rem;
+  width: 100%;
 
   @media ${QUERIES.laptopAndUp} {
     flex-direction: row;
-    flex: 0;
+    flex-wrap: wrap;
+    padding: 0 64px;
   }
 `;
 
@@ -86,19 +96,41 @@ const Title = styled.h2`
   text-align: start;
   font-size: ${FONT_STYLES.primaryTitles.size};
   font-weight: ${FONT_STYLES.primaryTitles.weight};
-  padding: 0 0.5rem;
+  text-transform: uppercase;
+  padding: 0 8px;
+
+  @media ${QUERIES.laptopAndUp} {
+    font-size: calc(48rem / 16);
+    padding: 3rem 8px 4rem;
+  }
 `;
 
 const ItemContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 3rem;
+  }
+`;
+
+const ItemTextContainer = styled.div`
+  @media ${QUERIES.laptopAndUp} {
+    width: 50%;
+  }
 `;
 
 const Image = styled.img`
   object-fit: cover;
-  min-width: 100%;
+  min-width: 50%;
+  max-width: 100%;
   height: auto;
   padding: 8px;
+
+  @media ${QUERIES.laptopAndUp} {
+    max-width: 50%;
+    height: 100%;
+  }
 `;
 
 const ItemTitle = styled.h3`
@@ -106,6 +138,10 @@ const ItemTitle = styled.h3`
   font-weight: ${FONT_STYLES.secondaryTitles.weight};
   padding: 0 0.5rem;
   line-height: 1.2;
+
+  @media ${QUERIES.laptopAndUp} {
+    font-size: calc(48rem / 16);
+  }
 `;
 
 const Text = styled.p`
@@ -114,13 +150,21 @@ const Text = styled.p`
   padding: 0.5rem;
   text-align: justify;
   line-height: 1.2;
+
+  @media ${QUERIES.laptopAndUp} {
+    font-size: calc(18rem / 16);
+  }
 `;
 
 const Conclusion = styled.p`
   padding: 0.5rem;
-  width: 75dvw;
+  min-width: 75%;
   font-weight: 700;
   line-height: 1.2;
+
+  @media ${QUERIES.laptopAndUp} {
+    font-size: calc(20rem / 16);
+  }
 `;
 
 const BookLink = styled(Link)`
