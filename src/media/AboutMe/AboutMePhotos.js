2b735af0862a539790384163a54c729a98edb550
Maximilien Brunet <maximilien.brunet@me.com>
2024-07-04 17:31:19 +0200

    added new photos

--- src/media/AboutMe/AboutMePhotos.js
+++ src/media/AboutMe/AboutMePhotos.js
@@ -1,4 +1,4 @@
 export { default as AboutMePhoto1 } from "./AboutMePhoto1.JPG";
 export { default as AboutMePhoto2 } from "./AboutMePhoto2.JPG";
-export { default as AboutMePhoto3 } from "./AboutMePhoto3.JPG";
+export { default as AboutMePhoto3 } from "./AboutMePhoto3.jpeg";
 export { default as AboutMePhoto4 } from "./AboutMePhoto4.jpeg";
