4dbee8189e7c821dfbecfaf57e3966605b199209
Maximilien Brunet <maximilien.brunet@me.com>
2024-09-19 16:51:02 +0200

    added photos via cloudinary for homepage and alts

--- src/components/HomePhotoCarousel.jsx
+++ src/components/HomePhotoCarousel.jsx
@@ -1,16 +1,9 @@
 import { useRef } from "react";
 import { register } from "swiper/element/bundle";
 import { useLayoutEffect as useIsomorphicLayoutEffect } from "react";
-register();
+import { BREAKPOINTS } from "../constants.js";
 
-//MEDIA IMPORTS
-import {
-  HomeCarouselPhoto1,
-  HomeCarouselPhoto2,
-  HomeCarouselPhoto3,
-  HomeCarouselPhoto4,
-  HomeCarouselPhoto5,
-} from "../media/HomeCarousel/HomeCarouselPhotos.js";
+register();
 
 export default function HomePhotoCarousel() {
   const swiperRef = useRef(null);
@@ -61,7 +54,6 @@ export default function HomePhotoCarousel() {
     ],
   };
 
-  //using useIsomorphicLayoutEffect to remove breakpoints bug
   useIsomorphicLayoutEffect(() => {
     if (swiperRef.current) {
       if (swiperRef.current?.swiper?.currentBreakpoint) {
@@ -83,19 +75,133 @@ export default function HomePhotoCarousel() {
   return (
     <swiper-container style={swiperContainer} ref={swiperRef} init="false">
       <swiper-slide>
-        <img src={HomeCarouselPhoto1} loading="lazy" alt="" />
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726752161/HomeCarouselPhoto1_ss6dm9.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726752161/HomeCarouselPhoto1_ss6dm9.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726752161/HomeCarouselPhoto1_ss6dm9.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726752161/HomeCarouselPhoto1_ss6dm9.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <img
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726752161/HomeCarouselPhoto1_ss6dm9.jpg"
+            loading="lazy"
+            alt="A Visites Parfums Paris tour participant smelling a perfume in a niche Parisian perfume shop"
+          />
+        </picture>
       </swiper-slide>
+
       <swiper-slide>
-        <img src={HomeCarouselPhoto2} loading="lazy" alt="" />
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726752163/HomeCarouselPhoto2_ybwgrm.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726752163/HomeCarouselPhoto2_ybwgrm.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726752163/HomeCarouselPhoto2_ybwgrm.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726752163/HomeCarouselPhoto2_ybwgrm.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <img
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726752163/HomeCarouselPhoto2_ybwgrm.jpg"
+            loading="lazy"
+            alt="A group of Visites Parfums Paris participants posing with Natacha Pascalis, perfumery expert and creator of the guided tours, in a passageway on rue des Francs Bourgeois in the Marais"
+          />
+        </picture>
       </swiper-slide>
+
       <swiper-slide>
-        <img src={HomeCarouselPhoto3} loading="lazy" alt="" />
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726752165/HomeCarouselPhoto3_ffeyww.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726752165/HomeCarouselPhoto3_ffeyww.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726752165/HomeCarouselPhoto3_ffeyww.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726752165/HomeCarouselPhoto3_ffeyww.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <img
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726752165/HomeCarouselPhoto3_ffeyww.jpg"
+            loading="lazy"
+            alt="A Visites Parfums Paris tour participant inspecting a beauty product in a niche Parisian perfume shop"
+          />
+        </picture>
       </swiper-slide>
+
       <swiper-slide>
-        <img src={HomeCarouselPhoto4} loading="lazy" alt="" />
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726752168/HomeCarouselPhoto4_ghotpg.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726752168/HomeCarouselPhoto4_ghotpg.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726752168/HomeCarouselPhoto4_ghotpg.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726752168/HomeCarouselPhoto4_ghotpg.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <img
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726752168/HomeCarouselPhoto4_ghotpg.jpg"
+            loading="lazy"
+            alt="A Visites Parfums Paris participant inspecting a perfume bottle display in a niche Parisian shop"
+          />
+        </picture>
       </swiper-slide>
+
       <swiper-slide>
-        <img src={HomeCarouselPhoto5} loading="lazy" alt="" />
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726752159/HomeCarouselPhoto5_znhvyp.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726752159/HomeCarouselPhoto5_znhvyp.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726752159/HomeCarouselPhoto5_znhvyp.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726752159/HomeCarouselPhoto5_znhvyp.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <img
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726752159/HomeCarouselPhoto5_znhvyp.jpg"
+            loading="lazy"
+            alt="A group of Visites Parfums Paris participants posing with Natacha Pascalis, perfumery expert and creator of the guided tours, in front of a historic entrance door in central Paris"
+          />
+        </picture>
       </swiper-slide>
     </swiper-container>
   );
--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,21 +1,138 @@
 import styled from "styled-components";
-import { QUERIES } from "../constants";
-import {
-  PhotoGalleryImage1,
-  PhotoGalleryImage2,
-  PhotoGalleryImage3,
-  PhotoGalleryImage4,
-  PhotoGalleryImage5,
-} from "../media/PhotoGallery/PhotoGalleryImages.js";
+import { QUERIES, BREAKPOINTS } from "../constants";
 
 export default function PhotoGallery() {
   return (
     <Wrapper>
-      <IMG src={PhotoGalleryImage1} loading="lazy" alt="PhotoGalleryImage1" />
-      <IMG src={PhotoGalleryImage2} loading="lazy" alt="PhotoGalleryImage2" />
-      <IMG src={PhotoGalleryImage3} loading="lazy" alt="PhotoGalleryImage3" />
-      <IMG src={PhotoGalleryImage4} loading="lazy" alt="PhotoGalleryImage4" />
-      <IMG src={PhotoGalleryImage5} loading="lazy" alt="PhotoGalleryImage5" />
+      <PictureContainer>
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726692185/PhotoGalleryImage1_enrfpo.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726692185/PhotoGalleryImage1_enrfpo.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726692185/PhotoGalleryImage1_enrfpo.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726692185/PhotoGalleryImage1_enrfpo.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <IMG
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726692185/PhotoGalleryImage1_enrfpo.jpg"
+            loading="lazy"
+            alt="A young woman's hand holding scented paper blotters in front of a historic Parisian door"
+          />
+        </picture>
+      </PictureContainer>
+
+      <PictureContainer>
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726692181/PhotoGalleryImage2_bpllbt.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726692181/PhotoGalleryImage2_bpllbt.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726692181/PhotoGalleryImage2_bpllbt.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726692181/PhotoGalleryImage2_bpllbt.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <IMG
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726692181/PhotoGalleryImage2_bpllbt.jpg"
+            loading="lazy"
+            alt="A typical outdoor table and chair at a Parisian café"
+          />
+        </picture>
+      </PictureContainer>
+
+      <PictureContainer>
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726692182/PhotoGalleryImage3_o2dotc.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726692182/PhotoGalleryImage3_o2dotc.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726692182/PhotoGalleryImage3_o2dotc.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726692182/PhotoGalleryImage3_o2dotc.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <IMG
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726692182/PhotoGalleryImage3_o2dotc.jpg"
+            loading="lazy"
+            alt="A large balcony with chessboard tile floors at Musée de la Marine in Paris"
+          />
+        </picture>
+      </PictureContainer>
+
+      <PictureContainer>
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726692183/PhotoGalleryImage4_ubg7bf.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726692183/PhotoGalleryImage4_ubg7bf.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726692183/PhotoGalleryImage4_ubg7bf.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726692183/PhotoGalleryImage4_ubg7bf.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <IMG
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726692183/PhotoGalleryImage4_ubg7bf.jpg"
+            loading="lazy"
+            alt="A flower on a white background"
+          />
+        </picture>
+      </PictureContainer>
+
+      <PictureContainer>
+        <picture>
+          <source
+            srcSet="
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1600,f_auto,q_auto/v1726692179/PhotoGalleryImage5_slbyrq.jpg 1600w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_1100,f_auto,q_auto/v1726692179/PhotoGalleryImage5_slbyrq.jpg 1100w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_770,f_auto,q_auto/v1726692179/PhotoGalleryImage5_slbyrq.jpg 770w,
+              https://res.cloudinary.com/dqs3mkxnr/image/upload/w_550,f_auto,q_auto/v1726692179/PhotoGalleryImage5_slbyrq.jpg 550w
+            "
+            sizes="
+              (min-width: ${BREAKPOINTS.desktopMin}px) 1500px,
+              (min-width: ${BREAKPOINTS.laptopMin}px) 1100px,
+              (min-width: ${BREAKPOINTS.bigTabletMin}px) 770px,
+              (min-width: ${BREAKPOINTS.tabletMin}px) 550px,
+              100vw
+            "
+            type="image/webp"
+          />
+          <IMG
+            src="https://res.cloudinary.com/dqs3mkxnr/image/upload/w_800/v1726692179/PhotoGalleryImage5_slbyrq.jpg"
+            loading="lazy"
+            alt="A woman spraying perfume on herself"
+          />
+        </picture>
+      </PictureContainer>
     </Wrapper>
   );
 }
@@ -24,19 +141,22 @@ const Wrapper = styled.section`
   display: flex;
   overflow-x: auto;
   white-space: nowrap;
-  width: 100dvw;
+  width: 100vw;
   margin-left: calc(-50vw + 50%);
 `;
 
-const IMG = styled.img`
+const PictureContainer = styled.div`
   flex: 1 0 auto;
   min-width: 10%;
   max-width: 30%;
   height: auto;
-  aspect-ratio: 1 / 1;
-  object-fit: cover;
 
   @media ${QUERIES.bigTabletAndUp} {
     width: 20%;
   }
+`;
+
+const IMG = styled.img`
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
 `;
