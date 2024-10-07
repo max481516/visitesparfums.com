605ebb167c4b1b9e00abb317914d18f51715325d
Maximilien Brunet <maximilien.brunet@me.com>
2024-10-07 22:15:10 +0200

    added sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    sitemap({
+      hostname: "https://vpparis.fr",
+      outDir: "dist",
+    }),
     react({
       babel: {
         plugins: [
