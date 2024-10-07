9ae6d3624d949ab820931656224476b690df59b8
Maximilien Brunet <maximilien.brunet@me.com>
2024-10-07 22:45:54 +0200

    trying to configure sitemap correctly

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import sitemap from "vite-plugin-sitemap";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     sitemap({
       hostname: "https://vpparis.fr",
       outDir: "dist",
+      extraUrls: ["/about", "/booking"],
     }),
     react({
       babel: {
