f19b586e4dbab2d19dea40739b3bbd9d7390895f
Maximilien Brunet <maximilien.brunet@me.com>
2024-10-16 11:57:59 +0200

    implmenting hotjar

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
 import AboutMe from "../components/AboutMe";
 import { Helmet } from "react-helmet-async";
 
-export default function about() {
+export default function About() {
+  useEffect(() => {
+    (function (h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function () {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: 5173492, hjsv: 6 };
+      a = o.getElementsByTagName("head")[0];
+      r = o.createElement("script");
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+  }, []);
+
   return (
     <>
       <Helmet>
