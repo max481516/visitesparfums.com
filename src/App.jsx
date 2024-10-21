b58dbd3571b792831af90748a150062bf9ba20f0
Maximilien Brunet <maximilien.brunet@me.com>
2024-10-21 17:57:40 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages";
 import About from "./pages/about";
@@ -8,7 +14,24 @@ import "./i18n/i18n";
 import ContactModal from "./components/ContactModal";
 import ContactForm from "./components/ContactForm";
 import useFavicon from "./hooks/useFavicon";
-import { useEffect } from "react";
+
+// This component will track route changes and push events to GTM
+function GTMPageViewTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const sendGtmPageView = () => {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: "pageview", // Use a custom event name
+        page: location.pathname, // Send current page path
+      });
+    };
+    sendGtmPageView();
+  }, [location]); // Re-run every time location (page) changes
+
+  return null; // This component only handles side effects, no need to render anything
+}
 
 function App() {
   useFavicon(); //for dynamic favicon
@@ -27,53 +50,15 @@ function App() {
       document.body.appendChild(noscript);
     };
 
-    // Load Yandex Metrika
-    const loadYandexMetrika = () => {
-      const script = document.createElement("script");
-      script.async = true;
-      script.src = "https://mc.yandex.ru/metrika/tag.js";
-      document.body.appendChild(script);
-
-      const inlineScript = document.createElement("script");
-      inlineScript.innerHTML = `
-        (function(m,e,t,r,i,k,a){
-          m[i]=m[i]||function(){
-            (m[i].a=m[i].a||[]).push(arguments)
-          };
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {
-            if (document.scripts[j].src === r) { return; }
-          }
-          k=e.createElement(t), a=e.getElementsByTagName(t)[0],
-          k.async=1, k.src=r, a.parentNode.insertBefore(k,a)
-        })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-  
-        ym(98578666, "init", {
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true,
-          webvisor:true
-        });
-      `;
-      document.body.appendChild(inlineScript);
-    };
-
-    // Load both Google Tag Manager and Yandex Metrika
     loadGTM();
-    loadYandexMetrika();
-
-    // Yandex.Metrika noscript workaround
-    const noscript = document.createElement("noscript");
-    noscript.innerHTML = `<div><img src="https://mc.yandex.ru/watch/98578666" style="position:absolute; left:-9999px;" alt="" /></div>`;
-    document.body.appendChild(noscript);
   }, []);
 
   return (
     <main>
       <Router>
+        <GTMPageViewTracker />
         <Navbar />
         <ContactModal>
-          {/* to ensure that modal opens on every page */}
           <ContactForm />
         </ContactModal>
         <Routes>
