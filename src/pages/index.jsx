ea3e4b60c1048ac2a0a8b0664d8e97f344e8786e
Maximilien Brunet <maximilien.brunet@me.com>
2024-11-04 21:28:32 +0100

    added structured data

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,10 +11,51 @@ import ReasonsWhyCarousel from "../components/ReasonsWhyCarousel.jsx";
 import { QUERIES } from "../constants.js";
 
 export default function Home() {
+  const structuredData = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "Visites Parfums Paris",
+    url: "https://vpparis.fr",
+    logo: "https://vpparis.fr/logo.svg",
+    sameAs: [
+      "https://www.instagram.com/visitesparfumsparis/",
+      "https://www.airbnb.fr/experiences/4256832?_set_bev_on_new_domain=1726611939_EAMmY4MWY5NjRiMj",
+      "https://www.airbnb.fr/experiences/4431379?_set_bev_on_new_domain=1726611939_EAMmY4MWY5NjRiMj",
+    ],
+    description:
+      "Discover unique perfume tours in Paris with Visites Parfums Paris. Personalized scent experiences in the heart of the city.",
+    founder: {
+      "@type": "Person",
+      name: "Natacha Pascalis",
+    },
+    contactPoint: {
+      "@type": "ContactPoint",
+      contactType: "Customer Support",
+      areaServed: "FR",
+      availableLanguage: ["French", "English", "Russian"],
+    },
+    makesOffer: {
+      "@type": "Offer",
+      name: "Perfume Tour",
+      url: "https://www.airbnb.fr/experiences/4256832?_set_bev_on_new_domain=1726611939_EAMmY4MWY5NjRiMj",
+      priceCurrency: "EUR",
+      price: "25",
+      eligibleRegion: {
+        "@type": "Place",
+        name: "Paris",
+      },
+      description:
+        "A guided tour exploring exclusive perfume boutiques and discovering unique fragrances.",
+    },
+  };
+
   return (
     <>
       <Helmet>
         <title>Visites Parfums Paris</title>
+        <script type="application/ld+json">
+          {JSON.stringify(structuredData)}
+        </script>
         <meta
           name="description"
           content="Experience guided tours of Paris's top perfume shops with Visites Parfums Paris. Discover exquisite fragrances and learn the fascinating history behind each scent."
